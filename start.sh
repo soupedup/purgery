7ba650d0c526318b7a0197ddb96d6daef8ca2c95
Joshua Sierles <joshua@chatterbug.com>
2021-06-03 06:05:37 +0200

    remove app-specific naming

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-PURGERY_ID=${FLY_ALLOC_ID} VARNISH_ADDR=${FLY_REGION}.${PROXY_APP_NAME}.internal:80 /usr/local/bin/purgery
+PURGERY_ID=${FLY_ALLOC_ID}
+VARNISH_ADDR=${FLY_REGION}.${PROXY_APP_NAME}.internal:80
+/usr/local/bin/purgery
