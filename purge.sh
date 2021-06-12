bd0654a375e234422b6d42aa0a26cad0a2458c3a
Panagiotis Siatras <azazeal@users.noreply.github.com>
2021-06-12 22:07:00 +0300

    fixed: purge.sh

--- purge.sh
+++ purge.sh
@@ -2,11 +2,11 @@
 
 #MINID=$(date +%s)
 #MINID=$(expr ${MINID} - 86400000)-0
-#redis-cli -u $REDIS_URL XADD purgery:purge MINID \~ ${MINID} \* url http://www.example.com
+#redis-cli -u $REDIS_URL XADD purgery:purge MINID \~ ${MINID} \* url ${1}
 
 curl \
     -X POST \
     -H "Content-Type: application/json" \
     -d "{ \"url\": \"${1}\" }" \
-    -u "${API_KEY}123": \
+    -u "${API_KEY}": \
     http://localhost:3000/purge
